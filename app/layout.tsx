80adf6d062d4c8caae61700efce9d11163cb6ac8
Felipe Silva <felipewrsilva@gmail.com>
2024-05-09 00:40:35 -0300

    Rename project title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const ibmPlexSerif = IBM_Plex_Serif({
   variable: '--font-ibm-plex-serif'
 })
 export const metadata: Metadata = {
-  title: "Horizon",
-  description: "Horizon is a modern baking platform for everyone.",
+  title: "FinWiz",
+  description: "FinWiz is a modern baking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
   }
